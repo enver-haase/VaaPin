4511916c1e11c5d695f025707dd21f660562d67f
Enver Haase <enver@vaadin.com>
2016-01-11 18:09:45 +0100

    fix TAB -> spaces

--- src/com/infraleap/vaapin/config/Configuration.java
+++ src/com/infraleap/vaapin/config/Configuration.java
@@ -5,7 +5,7 @@ public interface Configuration {
     public final static int TELNET_PORT = 9999;
     public final static int HIGHSCORES_PORT = 10000;
     
-	public final static String MACHINE_NAME = "www.hacenet";
+    public final static String MACHINE_NAME = "www.hacenet";
     public final static String HIGH_SCORES_URL = "http://" + MACHINE_NAME + ":" + HIGHSCORES_PORT;
 
     public final static String LOGIN_NAME = "Pin2000";
